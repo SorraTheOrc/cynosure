3295b4998381615c383f29f0be7b0cbb09887bf4
Ross Gardler <ross@gardler.org>
2017-05-13 01:00:29 -0700

    Move the help dialog over to using buttons

--- cynosure/Dialogs/HelpDialog.cs
+++ cynosure/Dialogs/HelpDialog.cs
@@ -14,15 +14,26 @@ namespace cynosure.Dialogs
     {
         public async Task StartAsync(IDialogContext context)
         {
-            String help = "My main resonsibility is to run your standup for you.\n\n";
-            help += "My commands:\n\n\n\n";
+            string title = "My main resonsibility is to run your standup for you.";
+            string help = "My commands:\n\n\n\n";
             help += "'start standup'   : Starts the standup conversation.\n\n";
             help += "'standup summary' : Displays a summary of the current standup reports";
 
             var reply = context.MakeMessage();
-            reply.Text = help;
-            reply.Speak = help;
+            reply.Speak = title + "\n\n" + help;
             reply.InputHint = InputHints.AcceptingInput;
+
+            reply.Attachments = new List<Attachment>
+            {
+                new HeroCard(title)
+                {
+                    Buttons = new List<CardAction>
+                    {
+                        new CardAction(ActionTypes.ImBack, "Start Standup", value: "start standup"),
+                        new CardAction(ActionTypes.ImBack, "Standup Summary", value: "standup summary")
+                    }
+                }.ToAttachment()
+            };
 
             await context.PostAsync(reply);
 
